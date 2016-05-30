71d49072d44642613f66cd331d30ed55bb525708
Rohit Reddy Karnaty <karnaty.rohit@gmail.com>
2016-05-30 17:13:24 +0530

    poisson is done

--- poisson.py
+++ poisson.py
@@ -5,12 +5,19 @@
 from scipy import sparse
 from readinput import *
 from dummy import dummy
-
+from dummy_prime import dummy_prime
 
 def poisson(spNd, spFn, Ec_old, F_prime, div_avd, charge_fac, Eg1, Eg2, Es, Ed, Nx, Ny, Ntotal):
     Temp = Te
     transport_model = transportmodel.value
     fermi_flag = fermiflag1.value
+
+    Lsda = round(Lsd/dx)
+    Lg_topa = round(Lg_top/dx)
+    Lg_bota = round(Lg_bot/dx)
+    t_topa = round(t_top/dy)
+    t_bota = round(t_bot/dy)
+    t_sia = round(t_si/dy)
 
     ###########################INPUT AND OUTPUT VARIABLES######################
     # spNd is the 3d net dopant density
@@ -56,116 +63,233 @@ def poisson(spNd, spFn, Ec_old, F_prime, div_avd, charge_fac, Eg1, Eg2, Es, Ed, 
 
         ####################THE START OF DUMMY VARIABLE DEFINITION#################
         dummy_fun = charge_fac * ((Nd+div_avdt)/Nc - dummy((Fn-Ec)/(k_B*Temp/q), dummy_flag,fermi_flag))
-        dummy_fun_prime = charge_fac * dummy_prime((Fn_real-Ec)/(k_B*Temp/q), dummy_flag,fermi_flag)/(k_B*Temp/q)
+        dummy_fun_prime = charge_fac * dummy_prime((Fn-Ec)/(k_B*Temp/q), dummy_flag,fermi_flag)/(k_B*Temp/q)
 
 
         ########################THE END OF DUMMY VARIABLE DEFINITION###############
 
-        if ox_pnt_flag == 0: # NO ELECTRON PENETRATION INTO OXIDE REGIONS:
+        if ox_pnt_flag == 0: # NO ELECTRON PENETRATION INTO OXIDE REGIONS
 
-            #################################EVALUATE F################################
-            # hpal: modified - F calculated using F_prime
-            F = full(F_prime*Ec)
+    #################################EVALUATE F#########################################
 
-            # Fill in the boundary conditions
-            for idex in range (Lsda+1,Lsda+Lg_topa+2):
-                F[idex-1] = Ec[idex-1]-Eg1
+    #############################Top gate insulator region##############################
+            for i in range(0,Nx*(t_topa+1)):
+                if(i >= 0 and i < Lsda):
+                    F[i] = Ec[i]-Ec[i+Nx]
+                elif(i >= Lsda and i <= (Lsda+Lg_topa)):
+                    F[i] = Ec[i]-Eg1
+                elif(i >= (Lsda+Lg_topa)+1 and i<Nx):
+                    F[i] = Ec[i]-Ec[i+Nx]
+                elif(i >= (Nx*t_topa) and i < Nx*(t_topa+1)):
+                    F[i] = -1/8*(dy/dx)*eps_top/eps_si*Ec[i-Nx-1] \
+                        - (dx/dy-1/4/(dx/dy))*eps_top/eps_si*Ec[i-Nx] \
+                        - 1/8*(dy/dx)*eps_top/eps_si*Ec[i-Nx+1] \
+                        - 3/8*(dy/dx)*(eps_top+eps_si)/eps_si*Ec[i-1] \
+                        + (dx/dy+3/4/(dx/dy))*(eps_top+eps_si)/eps_si*Ec[i] \
+                        - 3/8*(dy/dx)*(eps_top+eps_si)/eps_si*Ec[i+1] \
+                        - 1/8*(dy/dx)*Ec[i+Nx-1] \
+                        - (dx/dy-1/4/(dx/dy))*Ec[i+Nx] \
+                        - 1/8*(dy/dx)*Ec[i+Nx+1]
+                else:
+                    F[i] = -(dx/dy)*eps_top/eps_si*Ec[i-Nx] \
+                        -(dy/dx)*eps_top/eps_si*Ec[i-1] \
+                        +2*(dx/dy+dy/dx)*eps_top/eps_si*Ec[i] \
+                        -(dy/dx)*eps_top/eps_si*Ec[i+1] \
+                        -(dx/dy)*eps_top/eps_si*Ec[i+Nx]
 
-            for idex in range ((Ntotal-Nx+1+Lsda),(Ntotal-Lsda+1)):
-                F[idex-1] = Ec[idex-1]-Eg2
+            #########################Bottom gate insulator region##############################
+            for i in range((Ntotal-Nx*(t_bota+1)),Ntotal):
+                if(i >= (Ntotal-Nx*(t_bota+1)) and i<(Ntotal-Nx*t_bota)):
+                    F[i] = -1/8*(dy/dx)*Ec[i-Nx-1] \
+                        - (dx/dy-1/4/(dx/dy))*Ec[i-Nx] \
+                        - 1/8*(dy/dx)*Ec[i-Nx+1] \
+                        - 3/8*(dy/dx)*(eps_bot+eps_si)/eps_si*Ec[i-1] \
+                        + (dx/dy+3/4/(dx/dy))*(eps_bot+eps_si)/eps_si*Ec[i] \
+                        - 3/8*(dy/dx)*(eps_bot+eps_si)/eps_si*Ec[i+1] \
+                        - 1/8*(dy/dx)*eps_bot/eps_si*Ec[i+Nx-1] \
+                        - (dx/dy-1/4/(dx/dy))*eps_bot/eps_si*Ec[i+Nx] \
+                        - 1/8*(dy/dx)*eps_bot/eps_si*Ec[i+Nx+1]
+                elif(i >= (Ntotal-Nx) and i < (Ntotal-Nx+Lsda)):
+                    F[i] = Ec[i]-Ec[i-Nx]
+                elif(i >= (Ntotal-Nx+Lsda) and i<=(Ntotal-Nx+Lsda+Lg_bota)):
+                    F[i] = Ec[i]-Eg2
+                elif(i >= (Ntotal-Nx+1+Lsda+Lg_bota) and i < Ntotal):
+                    F[i] = Ec[i]-Ec[i-Nx]
+                else:
+                    F[i] = -(dx/dy)*eps_bot/eps_si*Ec[i-Nx] \
+                          - (dy/dx)*eps_bot/eps_si*Ec[i-1] \
+                          + 2*(dx/dy+dy/dx)*eps_bot/eps_si*Ec[i] \
+                          - (dy/dx)*eps_bot/eps_si*Ec[i+1] \
+                          - (dx/dy)*eps_bot/eps_si*Ec[i+Nx]
 
-            # Fill in the non-boundary area
-            for i in range ((t_topa+1),(t_topa+t_sia)):
-                for j in range(2,Nx):
-                    idex = Nx*i+j
-                    F[idex-1] += dummy_fun(idex)
+            #####################Specify the F matrix in the silicon film region################
+            for i in range(Nx*(t_topa+1),(Ntotal-Nx*(t_bota+1)+1)-1):
+                F[i] = -(dx/dy)*Ec[i-Nx]-(dy/dx)*Ec[i-1]+2*(dx/dy+dy/dx)*Ec[i]+dummy_fun[i]-(dy/dx)*Ec[i+1]-(dx/dy)*Ec[i+Nx]
 
-            ###############################EVALUATE MF_prime###########################
-            # MF_prime matrix in the silicon film region
-            for j_row in range (Nx*(t_topa+1)/Nx,(Ntotal-Nx*t_bota)/Nx-1):
-                for j_col in range (2,Nx):
-                    ii = j_row * Nx + j_col
-                    MF_prime[ii-1,ii-1] = dummy_fun_prime(ii)
+            #***************`*Modify the F matrix at the right and left boundaries***************
+            i_l = 0
+            i_r = Nx-1
+            for j in range(0,Ny):
+                if j == 0:
+                    F[i_l]=2*Ec[i_l]-Ec[i_l+1]-Ec[i_l+Nx]
+                    F[i_r]=2*Ec[i_r]-Ec[i_r-1]-Ec[i_r+Nx]
 
-        elif ox_pnt_flag == 1:# ACCOUNTING FOR ELECTRON PENETRATION INTO OXIDE REGIONS
+                elif(j > 0 and j < (round(Nx*(t_topa+1)/Nx)) - 1):
+                    F[i_l]=Ec[i_l]-Ec[i_l+1]
+                    F[i_r]=Ec[i_r]-Ec[i_r-1]
 
-            #################################EVALUATE F################################
-            # hpal: modified - F calculated using F_prime
-            F = full(F_prime*Ec)
+                elif(j >= round(Nx*(t_topa)/Nx) and j < round(Ntotal-Nx*t_bota/Nx)):
+                    F[i_l]=Ec[i_l]-Ec[i_l+1]
+                    F[i_r]=Ec[i_r]-Ec[i_r-1]
 
-            for idex in range(Lsda+1,Lsda+Lg_topa+2):
-                F[idex-1] = Ec[idex-1]-Eg1
+                elif(j >= round(Ntotal-Nx*t_bota/Nx) and j < Ny-1):
+                    F[i_l]=Ec[i_l]-Ec[i_l+1]
+                    F[i_r]=Ec[i_r]-Ec[i_r-1]
 
-            for idex in range ((Ntotal-Nx+1+Lsda),(Ntotal-Lsda+1)):
-                F[idex-1] = Ec[idex-1]-Eg2
+                elif(j == Ny-1 and ((Ntotal-Nx+1)<(Ntotal-Nx+1+Lsda)) and ((Ntotal-Nx+1+Lsda+Lg_bota)<Ntotal)):
+                    F[i_l]=2*Ec[i_l]-Ec[i_l+1]-Ec[i_l-Nx]
+                    F[i_r]=2*Ec[i_r]-Ec[i_r-1]-Ec[i_r-Nx]
 
-            for i in range(1,Ny-1):
-                for j in range (2,Nx-1):
-                    idex = Nx * i + j
-                    F[idex-1]=F[idex-1]+dummy_fun[idex-1]
+                i_l = 1+j*Nx
+                i_r = (j+1)*Nx
 
-            #############################EVALUATE MF_prime#####################
-            #MF_prime matrix in the silicon film region
-            for idex in range (Nx+1,(Ntotal-Nx+1)):
-                MF_prime[idex-1,idex-1] = dummy_fun_prime[idex-1]
 
-            for jdex in range (2,Ny):
-                MF_prime[(jdex-1)*Nx,(jdex-1)*Nx] = 0
-                MF_prime[jdex*Nx-1,jdex*Nx-1] = 0
+    ##############################END OF EVALUATING F##################################
 
-        #######################################################################
-        # Debugger ##########
-        #######################################################################
-        # if debug == 1:
-        #     # Plot the contour of F
-        #     if debug_counter == 0:
-        #         debug_counter += 1
-        #         V=np.linspace(1,error_inner/criterion_inner,20)
-        #
-        #     F_2D = np.reshape(abs(np.real(F)),Nx,Ny)./criterion_inner #Rohit - check reshape compatability
-        #     figure(100)
-        #     contourf(F_2D, V)
-        #     colorbar
-        #
-        #     pause
-        # end
+    ###############################EVALUATE MF_prime###################################
+    # MF_prime matrix in the silicon film region
+            for j_row in range (Nx*(t_topa+1)/Nx, (Ntotal-Nx*t_bota)/Nx-2):
+                for j_col in range(1,Nx-1):
+                    ii = j_row*Nx+j_col
+                    MF_prime[ii,ii] = dummy_fun_prime[ii]
 
-        ##############END OF ELECTRON PENETRATION INTO OXIDE OPTION SWITCH#####
+    ###############END OF EVALUATION FOR NO PENETRATION INTO THE OXIDE#################
+
+        elif ox_pnt_flag == 1: #(ACCOUNTING FOR ELECTRON PENETRATION INTO OXIDE REGIONS)
+
+    #################################EVALUATE F########################################
+
+    ##########################Top gate insulator region################################
+            for i in range(0, Nx*(t_topa+1)):
+                if(i >= 0 and i < Lsda+1-1) :
+                    F[i] = Ec[i]-Ec[i+Nx]
+                elif(i >= Lsda and i < (Lsda+Lg_topa)+1):
+                    F[i] = Ec[i]-Eg1
+                elif(i >= (Lsda+Lg_topa)+1 and i<Nx):
+                    F[i] = Ec[i]-Ec[i+Nx]
+                elif(i >= (Nx*t_topa+1) and i <= Nx*(t_topa+1)):
+                    F[i] = -1/8*(dy/dx)*eps_top/eps_si*Ec[i-Nx-1] \
+                              - (dx/dy-1/4/(dx/dy))*eps_top/eps_si*Ec[i-Nx] \
+                              - 1/8*(dy/dx)*eps_top/eps_si*Ec[i-Nx+1] \
+                              - 3/8*(dy/dx)*(eps_top+eps_si)/eps_si*Ec[i-1] \
+                              + (dx/dy+3/4/(dx/dy))*(eps_top+eps_si)/eps_si*Ec[i] \
+                              + dummy_fun[i] \
+                              - 3/8*(dy/dx)*(eps_top+eps_si)/eps_si*Ec[i+1] \
+                              - 1/8*(dy/dx)*Ec[i+Nx-1] \
+                              - (dx/dy-1/4/(dx/dy))*Ec[i+Nx] \
+                              - 1/8*(dy/dx)*Ec[i+Nx+1]
+                else:
+                    F[i] = -(dx/dy)*eps_top/eps_si*Ec[i-Nx] \
+                            - (dy/dx)*eps_top/eps_si*Ec[i-1] \
+                            + 2*(dx/dy+dy/dx)*eps_top/eps_si*Ec[i]+dummy_fun[i] \
+                            - (dy/dx)*eps_top/eps_si*Ec[i+1] \
+                            - (dx/dy)*eps_top/eps_si*Ec[i+Nx]
+
+        ############################Bottom gate insulator region###########################
+            for i in range((Ntotal-Nx*(t_bota+1)),Ntotal):
+                if(i >= (Ntotal-Nx*(t_bota+1)) and i < Ntotal-Nx*t_bota):
+                    F[i] = -1/8*(dy/dx)*Ec[i-Nx-1] \
+                        - (dx/dy-1/4/(dx/dy))*Ec[i-Nx] \
+                        - 1/8*(dy/dx)*Ec[i-Nx+1] \
+                        - 3/8*(dy/dx)*(eps_bot+eps_si)/eps_si*Ec[i-1] \
+                        + (dx/dy+3/4/(dx/dy))*(eps_bot+eps_si)/eps_si*Ec[i] \
+                        + dummy_fun[i] \
+                        - 3/8*(dy/dx)*(eps_bot+eps_si)/eps_si*Ec[i+1] \
+                        - 1/8*(dy/dx)*eps_bot/eps_si*Ec[i+Nx-1] \
+                        - (dx/dy-1/4/(dx/dy))*eps_bot/eps_si*Ec[i+Nx] \
+                        - 1/8*(dy/dx)*eps_bot/eps_si*Ec[i+Nx+1]
+                elif(i >= (Ntotal-Nx) and i < (Ntotal-Nx+1+Lsda)-1):
+                    F[i] = Ec[i]-Ec[i-Nx]
+                elif(i >= (Ntotal-Nx+Lsda) and i < (Ntotal-Nx+1+Lsda+Lg_bota)):
+                    F[i] = Ec[i]-Eg2
+                elif(i >= (Ntotal-Nx+1+Lsda+Lg_bota) and i < Ntotal):
+                    F[i] = Ec[i]-Ec[i-Nx]
+                else:
+                    F[i] = -(dx/dy)*eps_bot/eps_si*Ec[i-Nx] \
+                        - (dy/dx)*eps_bot/eps_si*Ec[i-1] \
+                        + 2*(dx/dy+dy/dx)*eps_bot/eps_si*Ec[i] + dummy_fun[i] \
+                        - (dy/dx)*eps_bot/eps_si*Ec[i+1] \
+                        - (dx/dy)*eps_bot/eps_si*Ec[i+Nx]
+
+        ##################Specify the F matrix in the silicon film region#################
+            for i in range(Nx*(t_topa+1), (Ntotal-Nx*(t_bota+1)+1)-1):
+                F[i] = -(dx/dy)*Ec[i-Nx]-(dy/dx)*Ec[i-1]+2*(dx/dy+dy/dx)*Ec[i]+dummy_fun[i]-(dy/dx)*Ec[i+1]-(dx/dy)*Ec[i+Nx]
+
+        #***************Modify the F matrix at the right and left boundaries**************
+            i_l = 0
+            i_r = Nx-1
+            for j in range(0, Ny):
+                if j == 0:
+                    F[i_l] = 2*Ec[i_l]-Ec[i_l+1]-Ec[i_l+Nx]
+                    F[i_r] = 2*Ec[i_r]-Ec[i_r-1]-Ec[i_r+Nx]
+
+                elif (j > 0 and j < round(Nx*(t_topa+1)/Nx) - 1):
+                    F[i_l] = Ec[i_l]-Ec[i_l+1]
+                    F[i_r] = Ec[i_r]-Ec[i_r-1]
+
+                elif (j >= round(Nx*(t_topa)/Nx) and j < round(Ntotal-Nx*t_bota/Nx)):
+                    F[i_l] = Ec[i_l]-Ec[i_l+1]
+                    F[i_r] = Ec[i_r]-Ec[i_r-1]
+
+                elif (j >= round(Ntotal-Nx*t_bota/Nx) and j<Ny-1):
+                    F[i_l]=Ec[i_l]-Ec[i_l+1]
+                    F[i_r]=Ec[i_r]-Ec[i_r-1]
+
+                elif(j == Ny-1 and ((Ntotal-Nx+1)<(Ntotal-Nx+1+Lsda)) and ((Ntotal-Nx+1+Lsda+Lg_bota)<Ntotal)):
+                    F[i_l] = 2*Ec[i_l]-Ec[i_l+1]-Ec[i_l-Nx]
+                    F[i_r] = 2*Ec[i_r]-Ec[i_r-1]-Ec[i_r-Nx]
+
+                i_l = 1+j*Nx
+                i_r = (j+1)*Nx
+
+        ##########################END OF EVALUATING F###################################
+
+        #############################EVALUATE MF_prime##################################
+        # MF_prime matrix in the silicon film region
+            for i in range(Nx, (Ntotal-Nx+1)-1):
+                MF_prime[i, i] = dummy_fun_prime[i]
+
+            for j in range(1, (Ny-1)):
+                MF_prime[(j-1)*Nx+1, (j-1)*Nx+1] = 0
+                MF_prime[j*Nx, j*Nx] = 0
+        ###############END OF EVALUATION FOR PENETRATION INTO OXIDE ###################
+        ##############END OF ELECTRON PENETRATION INTO OXIDE OPTION SWITCH#############
 
         MF_prime = F_prime+sparse.csr_matrix(MF_prime)
+        #######################END OF EVALUATING MF_prime##############################
 
-        #######################END OF EVALUATING MF_prime######################
+        ############################SOLVING FOR delta_Ec###############################
+        delta_Ec = - np.linalg.solve(sparse.csr_matrix(MF_prime), sparse.csr_matrix(F))
 
-        ############################SOLVING FOR delta_Ec#######################
-        # XW - sometimes, MF_prime will become singular due to incorrect Nc
-        # value. Adjust Nc to avoid this problem.
-
-        #condition_MF_prime = condest(MF_prime)
-        #condition_F = cond(F)
-
-
-        delta_Ec = -sparse.csr_matrix(MF_prime)/sparse.csr_matrix(F)  #Rohit - \ or / ???
-
-        # nonlinear convergence
-        # XW - I do believe this is a limitation on line search to avoid
-        # singularity problem
-        for idex in range (1,Ntotal+1):
-            if abs(delta_Ec[idex-1]) <= 1:
-                delta_Ec[idex-1] = delta_Ec[idex-1]
-            elif 1 < abs(delta_Ec[idex-1]) and abs(delta_Ec[idex-1]) < 3.7:
-                delta_Ec[idex-1] = np.sign(delta_Ec[idex-1])*(abs(delta_Ec[idex-1]).^1/5)  # Rohit modify element wise multiplication from matlabn to python
-            elif abs(delta_Ec[idex-1]) >= 3.7:
-                delta_Ec[idex-1] = np.sign(delta_Ec[idex-1])*np.log(abs(delta_Ec[idex-1]))
-        ##########################END OF SOLVING FOR delta_Ec######################
+        for i in range(0, Ntotal):
+            if abs(delta_Ec(i)) <= 1:
+                delta_Ec[i] = delta_Ec[i]
+            elif 1<abs(delta_Ec(i)) and abs(delta_Ec(i)) <3.7:
+                delta_Ec[i] = np.sign(delta_Ec(i))*np.power(abs(delta_Ec(i)),0.20)
+            elif abs(delta_Ec(i)) >= 3.7:
+                delta_Ec[i] = np.sign(delta_Ec[i])*np.log(abs(delta_Ec[i]))
+        ##########################END OF SOLVING FOR delta_Ec#########################
 
         Ec = Ec+delta_Ec
         error_inner = max(abs(np.real(F)))
-        print '%s %e \n' % ('error_inner = ',error_inner)
-        max_delta_Ec = max(abs(np.real(full(delta_Ec))))
-        MF_prime = np.zeros(Ntotal,Ntotal)
+        print '%s %e \n' % ('error_inner = ', error_inner)
+        max_delta_Ec = max(abs(np.real(delta_Ec.toarray())))
+        MF_prime = np.zeros((Ntotal, Ntotal))
+        F = np.zeros((Ntotal, 1))
 
-    return Ec
+
     ##########################END OF INNER LOOP (WHILE) #######################
+    return  Ec
     ###########################################################################
     ###########################END OF FUNCTION POISSON#########################
     ###########################################################################
